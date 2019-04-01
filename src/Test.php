0227834dc00526d58e90705a14ca487e0c2c832e
chehuifang <chehuifang@100tal.com>
2019-04-01 19:16:05 +0800

    first package

---
+++ src/Test.php
@@ -0,0 +1,16 @@
+<?php
+namespace FirstPackage;
+
+class Test
+{
+    public function __construct()
+    {
+    }
+
+    public function testPackage()
+    {
+        return "first package";
+    }
+}
+
+
